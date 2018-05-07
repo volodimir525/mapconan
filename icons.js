68ebab19369917fb2b7c9c714d0dc40d39dc3c15
Karwisch <nak53188@gmail.com>
2018-05-07 01:18:25 -0400

    Map Visibility Size Change

--- icons.js
+++ icons.js
@@ -18,7 +18,7 @@ var coalIcon = L.icon({
 var brimstoneIcon = L.icon({
 	iconUrl: 'icons/icon_brimstone.png',
 
-	iconSize:	[32,32],
+	iconSize:	[48,48],
 	iconAnchor: [16,16],
 	popupAnchor:	[-10, -35]
 });
@@ -26,7 +26,7 @@ var brimstoneIcon = L.icon({
 var crystalIcon = L.icon({
 	iconUrl: 'icons/icon_crystal.png',
 				
-	iconSize:	[32,32],
+	iconSize:	[48,48],
 	iconAnchor: [16,16],
 	popupAnchor:	[-10, -35]
 });
