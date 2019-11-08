3cc85228e27aca5b7ad26248683f7b8c1f75d29f
volodimir525 <volodimirsv525@gmail.com>
2019-11-08 10:33:08 -0500

    case sensitive table name

--- backend/markerData.php
+++ backend/markerData.php
@@ -9,11 +9,20 @@
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}	
+		printf (json_encode(array()));
+		exit;
+		// die("Connection failed: " . $conn->connect_error);
+	}
 	
-    $sql = "SELECT * FROM markerdata";
+    $sql = "SELECT * FROM markerData";
     $result = $conn->query($sql);
+
+	if (!$result) {
+		printf (json_encode(array()));
+		exit;
+		// die("No result found");
+	}
+
 	$rows = array();
 	while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
 		$rows[] = $row;
